ea30fd645f255cd7e6e390ad64d61346dd4460ae
Jennifer Toops <jennifertoops@gmail.com>
2021-04-25 14:06:15 -0400

    added array practice average scores

--- 04-25-2021Codewars.js
+++ 04-25-2021Codewars.js
@@ -6,3 +6,31 @@ function sayHello( name, city, state ) {
     let newName = name.join(" ")
     return `Hello, ${newName}! Welcome to ${city}, ${state}!`
   }
+
+
+//Remove First and Last Character Part Two
+function array(str){
+    // given a CSV string
+    // return a string that looks like the first one, but the commas 
+    //           are replaced with spaces
+    // if empty string, or string < string.length ===3, return null
+      let length = str.length
+      str = str.replace(/\s/g,"")
+      let strArr = str.split(",")
+      console.log(strArr)
+      if (strArr.length < 3) {
+        return null
+        }
+       else {
+        strArr.shift()
+        strArr.pop()
+        console.log(strArr)
+        return strArr.join(" ")
+      }
+  }
+
+  //Average Scores (array method)
+  function average(scores) {
+    let averageScores = Math.round(scores.reduce((acc, c) => acc + c, 0) / scores.length)
+    return averageScores
+}
