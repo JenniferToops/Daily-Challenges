42597a8ee66c6207f83c90c4bd786132a6a6fa0a
Jennifer Toops <jennifertoops@gmail.com>
2021-04-25 08:46:59 -0400

    Simple validation of a username with regex

---
+++ 04-24-2021Codewars.js
@@ -0,0 +1,15 @@
+//Simple validation of a username with regex
+
+function validateUsr(username) {
+    //username needs to be 4-16 characters
+    //username can contain undercase letters
+    //username can contain numbers
+    //username can contain underscore
+    if (username.length < 4 || username.length > 17) { 
+      return false
+     } else {
+    const regex = new RegExp(/^[a-z0-9_]+$/)
+      res = regex.test(username)
+      }
+      return res
+  }
