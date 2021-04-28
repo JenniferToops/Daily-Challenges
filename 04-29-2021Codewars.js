bcb4a1574a53ab87b26fe59a5da2c526b4a5a226
Jennifer Toops <jennifertoops@gmail.com>
2021-04-28 12:46:19 -0400

    nameShuffler kata added

--- 04-29-2021Codewars.js
+++ 04-29-2021Codewars.js
@@ -9,3 +9,10 @@ var repeatIt = function(str, n) {
       return repeatedString
     }
   }
+
+//NameShuffler
+/* Write a function that returns a string in which firstname is swapped with last name.
+nameShuffler('john McClane'); => "McClane john"*/
+function nameShuffler(str){
+    return str.split(" ").reverse().join(" ")
+  }
