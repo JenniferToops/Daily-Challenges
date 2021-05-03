336fdbef5af8a68bdaaf4e9fce5a69396e28c6ef
Jennifer Toops <jennifertoops@gmail.com>
2021-05-02 22:36:37 -0400

    removing elements (array method)

---
+++ 05-02-2021Codewars.js
@@ -0,0 +1,20 @@
+//ARRAY METHOD
+//Removing Elements
+/* Take an array and remove every second element from the array. Always keep the first element and start removing with the next element.
+
+Example:
+
+["Keep", "Remove", "Keep", "Remove", "Keep", ...] --> ["Keep", "Keep", "Keep", ...]
+None of the arrays will be empty, so you don't have to worry about that! */
+
+function removeEveryOther(arr){
+    //remove every odd index
+    //will never have an empty array
+    //elements may be strings or numbers
+      let newArr = []
+      for (let i = 0; i < arr.length; i++) {
+      if (i % 2 === 0) {
+        newArr.push(arr[i])
+      }
+    } return newArr
+  }
