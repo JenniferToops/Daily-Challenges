1172389961fa8019225734198150dca15289436f
Jennifer Toops <jennifertoops@gmail.com>
2021-05-16 22:55:09 -0400

    added evens times last array methods

--- 05-16-2021Codewars.js
+++ 05-16-2021Codewars.js
@@ -12,3 +12,23 @@ function smallEnough(a, limit) {
     return true
     }
 }
+
+//Evens times last
+
+/* Given a sequence of integers, return the sum of all the integers that have an even index, multiplied by the integer at the last index.
+
+Indices in sequence start from 0.
+
+If the sequence is empty, you should return 0. */
+
+function evenLast(numbers) {
+    if (numbers.length < 1) {
+      return 0  
+    } else if (numbers.length > 0) {
+      let evens = numbers.filter((el, index) => index % 2 === 0)
+      console.log(evens)
+      let evensSum = evens.reduce((acc, c) => acc + c, 0)
+      console.log(evensSum)
+      return evensSum * numbers[numbers.length-1]
+    }  
+  }
