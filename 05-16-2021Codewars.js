d26890e2a419dbf7d2f5ec3d567cfa29a94ebf30
Jennifer Toops <jennifertoops@gmail.com>
2021-05-16 22:59:25 -0400

    added javascript array filter

--- 05-16-2021Codewars.js
+++ 05-16-2021Codewars.js
@@ -32,3 +32,15 @@ function evenLast(numbers) {
       return evensSum * numbers[numbers.length-1]
     }  
   }
+
+  //JavaScript Array Filter
+
+  /* JavaScript Arrays support a filter function (starting in JavaScript 1.6). Use the filter functionality to complete the function given.
+
+The solution would work like the following:
+
+getEvenNumbers([2,4,5,6]) // should == [2,4,6] */
+
+  function getEvenNumbers(numbersArray){
+    return numbersArray.filter(el => el % 2 === 0)
+  }
