a7fc1ce40d06dfea0ac4d1e580c5d21243c700b9
Jennifer Toops <jennifertoops@gmail.com>
2021-06-13 17:30:11 -0400

    type of sum

--- 06-13-2021codewars.js
+++ 06-13-2021codewars.js
@@ -15,3 +15,8 @@ function whoIsPaying(name){
       return [name]
   }
   }
+
+//   type of sum
+//Return the type of the sum of the two arguments
+const typeOfSum = (a, b) => (typeof a === "string") || (typeof b === "string") ? "string" : "number"
+
