261c5c9166cd6cec4bdf55e51ddb2c464de20d0d
Jennifer Toops <jennifertoops@gmail.com>
2021-06-16 13:32:22 -0400

    enumerable magic #3 = does my list include this?

--- 06-14-2021codewars.js
+++ 06-14-2021codewars.js
@@ -46,3 +46,22 @@ function sc(s){
       }   
     } return newArr.join("")
   }
+
+  /* Choo-king's alternate code:
+  function sc(s){
+  	//Create array for only upper case letters
+    let uppers=s.match(/[A-Z]/g); 
+  	//If there are no upper case letters, there are no father son pairs
+    if (uppers==null) return "";
+  	//Create array for only lower case letters
+    let lowers=s.match(/[a-z]/g);
+  	//If there are no lower case letters, there are no father son pairs
+    if (lowers==null) return "";
+  	//Filter lowers, keeping only the letters that appear in the uppers array
+  	//The resulting array contains all the father son paired letters in their lower case form
+    let fatherSonLetters=lowers.filter((elem)=>uppers.includes(elem.toUpperCase()));
+  	//Return empty string if there are no father son pairs
+    if (fatherSonLetters==null) return "";
+  	//Filter the original string, keeping only letters that are part of father son pairs and return the result
+    return(s.split('').filter((elem)=>fatherSonLetters.includes(elem.toLowerCase()))).join('');
+} */
---
+++ 06-16-2021codewars.js
@@ -0,0 +1,7 @@
+//Enumerable Magic #3 - Does My List Include This?
+
+function include(arr, item) {
+    return arr.includes(item)
+}
+
+const include = (arr, item) => arr.includes(item);
