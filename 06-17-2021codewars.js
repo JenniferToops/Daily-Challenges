03f671b73bcfed60c467ef703dfd738e01e27700
Jennifer Toops <jennifertoops@gmail.com>
2021-06-18 15:22:50 -0400

    simple calculator

---
+++ 06-17-2021codewars.js
@@ -0,0 +1,18 @@
+//simple calculator
+function calculator(a,b,sign){
+    if(typeof a !== "number" || typeof b !== "number") {
+      return "unknown value"  
+    } else if (sign === "+") {
+        return a + b
+      } else if (sign === "-") {
+        return a - b
+      } else if (sign === "/") {
+        return a / b
+      } else if (sign === "*") {
+        return a * b
+      } else if (sign === "%") {
+        return a % b
+      }    else {
+      return "unknown value" 
+    } 
+  }
