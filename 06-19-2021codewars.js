b0b7a5b4af74541faecce7a5dcdf1eb991101e25
Jennifer Toops <jennifertoops@gmail.com>
2021-06-19 19:16:42 -0400

    Simple Fun #176: Reverse Letter

--- 06-19-2021codewars.js
+++ 06-19-2021codewars.js
@@ -1,3 +1,13 @@
 //needle in a haystack -- reafactored using arrow function
 
 const findNeedle = haystack => `found the needle at position ${haystack.indexOf("needle")}`
+
+// Simple Fun #176: Reverse Letter
+
+function reverseLetter(str) {
+    let strArr = str.split("")
+    const regex = /[a-z]/gi
+    let onlyLettersArr = strArr.filter(el => el.match(regex))
+    let onlyLettersStr = onlyLettersArr.reverse().join("")
+    return onlyLettersStr
+  }
