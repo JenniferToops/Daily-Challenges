1f7f166fbaa9e7e5824dad499d066fa911eb9115
Jennifer Toops <jennifertoops@gmail.com>
2021-06-19 19:50:05 -0400

    refactored disemvowelled trolls

--- 06-19-2021codewars.js
+++ 06-19-2021codewars.js
@@ -11,3 +11,17 @@ function reverseLetter(str) {
     let onlyLettersStr = onlyLettersArr.reverse().join("")
     return onlyLettersStr
   }
+
+  //refactor Disemvowel Trolls
+
+  function disemvowel(str) {
+    //parameter is string
+    //return needs to be a new string with vowels removed
+    //example: "pretty baby needs a doll" returns "prtty bby nds dll"
+    //pseudo: regex with no vowels or regex with vowels removed (case insensitive)
+    const regex = /[aeiou]/gi
+    let strArr = str.split("")
+    let newStrArr = strArr.filter(el => !el.match(regex))
+    let strStr = newStrArr.join("")
+    return strStr
+  }
