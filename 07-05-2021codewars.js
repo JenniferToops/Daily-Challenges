a3988a713e8fdc8a07361064c407f5b61e12f635
Jennifer Toops <jennifertoops@gmail.com>
2021-07-05 18:52:35 -0400

    Slaphead:

--- 07-05-2021codewars.js
+++ 07-05-2021codewars.js
@@ -16,3 +16,44 @@ function generateShape(integer){
       }
     return arrString.join("\n")
   }
+
+/* Being a bald man myself, I know the feeling of needing to keep it clean shaven. Nothing worse that a stray hair waving in the wind.
+
+You will be given a string(x). Clean shaved head is shown as "-" and stray hairs are shown as "/". Your task is to check the head for stray hairs and get rid of them.
+
+You should return the original string, but with any stray hairs removed. Keep count ot them though, as there is a second element you need to return:
+
+0 hairs --> "Clean!"
+1 hair --> "Unicorn!"
+2 hairs --> "Homer!"
+3-5 hairs --> "Careless!"
+>5 hairs --> "Hobo!"
+
+So for this head: "------/------" you should return:
+
+["-------------", "Unicorn"] */
+
+
+
+  function bald(x){
+    let xLength = x.length
+    let str = "-"
+    let dashStr = str.repeat(xLength)
+    let xArray = x.split("")
+    let slashArr = xArray.filter(el => el === "/")
+    if (xArray.every(el => el === "-")) {
+      return [dashStr, "Clean!"]
+    } else if (slashArr.length === []) {
+      return [dashStr, "Clean!"]
+    }  else if (slashArr.length === 1) {
+      return [dashStr, "Unicorn!"]
+    } else if (slashArr.length === 2) {
+      return [dashStr, "Homer!"]
+    } else if (slashArr.length > 2 && slashArr.length <= 5) {
+      return [dashStr, "Careless!"]
+    } else {
+      return [dashStr, "Hobo!"]
+    }
+  }
+
+  
