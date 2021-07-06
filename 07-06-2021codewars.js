334a34b28b44a378f6f783e41cc9776f941f7720
Jennifer Toops <jennifertoops@gmail.com>
2021-07-06 11:02:41 -0400

    No oddities here

---
+++ 07-06-2021codewars.js
@@ -0,0 +1,12 @@
+/* Write a small function that returns the values of an array that are not odd.
+
+All values in the array will be integers. Return the good values in the order they are given. */
+
+// function noOdds( values ){
+//   // Return all non-odd values
+//   // pseudo return values/elements where mod 2 === 0 in array
+//   let even = values.filter(el => el % 2 === 0)
+//   return even
+// }
+
+const noOdds = values => values.filter(el => el % 2 === 0)
