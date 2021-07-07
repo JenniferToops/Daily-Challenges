f69c577c5925e267dd8eb519684b1c9b3dfe5928
Jennifer Toops <jennifertoops@gmail.com>
2021-07-07 11:09:49 -0400

    refactored Jenny's secret message

--- 07-07-2021codewars.js
+++ 07-07-2021codewars.js
@@ -6,3 +6,13 @@ Complete the function that takes a string as an argument representing the curren
 For example, update_light('green') should return 'yellow'. */
 
 const updateLight = current =>  current === "green" ? "yellow" : current === "yellow" ? "red" : "green"
+
+/*refactored: Jenny's secret Message 
+ Jenny has written a function that returns a greeting for a user. However, she's in love with Johnny, and would like to greet him slightly different. She added a special case to her function, but she made a mistake.
+
+Can you help her?
+ Test.assertEquals(greet("Jim"), "Hello, Jim!");
+ Test.assertEquals(greet("Johnny"), "Hello, my love!");
+ */
+
+ const greet = name => name === "Johnny" ? "Hello, my love!" : `Hello, ${name}!`
