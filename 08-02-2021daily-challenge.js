21a68a1b5e7f88581f3d35953ed001e62ad12c93
Jennifer Toops <jennifertoops@gmail.com>
2021-08-02 17:07:54 -0400

    isograms

--- 08-02-2021daily-challenge.js
+++ 08-02-2021daily-challenge.js
@@ -18,3 +18,28 @@ function findDeletedNumber(array1, mixArr) {
       missingNum = array1.filter(el => mixArr.includes(el) ? console.log(el) : missingNum.push(el) )
       } return Number(missingNum.join(""))
   }
+
+
+  /* Isograms
+An isogram is a word that has no repeating letters, consecutive or non-consecutive. Implement a function that determines whether a string that contains only letters is an isogram. Assume the empty string is an isogram. Ignore letter case.
+
+isIsogram("Dermatoglyphics") == true
+isIsogram("aba") == false
+isIsogram("moOse") == false // -- ignore letter case */
+
+
+function isIsogram(str){
+    let strArr = str.toLowerCase().split("")
+    let uniqueStrArr = [...new Set(str.toLowerCase())]
+   
+    if (strArr.join("") == uniqueStrArr.join("")) {
+      return true
+    } else {
+      return false  
+    }
+  }
+  
+  //using regex (not my code):
+  function isIsogram(str){ 
+    return !/(\w).*\1/i.test(str)
+  }
