3c5d355d45cc464dec3854ddc7f83700adc9ea90
Jennifer Toops <jennifertoops@gmail.com>
2021-08-06 17:53:10 -0400

    unique in order

--- 08-06-2021daily-challenge.js
+++ 08-06-2021daily-challenge.js
@@ -14,3 +14,36 @@ function averageLength(arr) {
     let avgElLength = Math.round(elLength.reduce((acc, c) => acc + c) / arr.length)
     return arr.map((el) => el[0].repeat(avgElLength))
   }
+
+  /* Alan Partridge III - London 
+  Ever the learned traveller, Alan Partridge has pretty strong views on London:
+
+"Go to London. I guarantee you'll either be mugged or not appreciated.
+Catch the train to London, stopping at Rejection, Disappointment, Backstabbing Central and Shattered Dreams Parkway."
+Your job is to check that the provided list of stations contains all of the stops Alan mentions. There will be other stations in the array. Example:
+
+['Rejection', 'Disappointment', 'Backstabbing Central', 'Shattered Dreams Parkway']
+If the stations all appear, return 'Smell my cheese you mother!', if not, return 'No, seriously, run. You will miss it. '.
+*/
+function alan(x){
+    let stations = ['Rejection', 'Disappointment', 'Backstabbing Central', 'Shattered Dreams Parkway']
+    if (x.includes(stations[0]) && x.includes(stations[1]) && x.includes(stations[2]) && x.includes(stations[3])) {
+      return 'Smell my cheese you mother!'
+    } else {
+      return 'No, seriously, run. You will miss it.'
+    }
+  }
+
+/* unique in order  Implement the function unique_in_order which takes as argument a sequence and returns a list of items without any elements with the same value next to each other and preserving the original order of elements.
+
+For example:
+
+uniqueInOrder('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B']
+uniqueInOrder('ABBCcAD')         == ['A', 'B', 'C', 'c', 'A', 'D']
+uniqueInOrder([1,2,2,3,3])       == [1,2,3] */
+
+function uniqueInOrder(iterable) {
+    let iterableArr = Array.from(iterable)
+    let uniques = []
+    return iterableArr.filter((el, i, arr) => arr[i] !== arr[i-1])
+    }
