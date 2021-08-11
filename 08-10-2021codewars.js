b687b02e775b5dd8fd3ffa98fc12a2a58b1bdbf1
Jennifer Toops <jennifertoops@gmail.com>
2021-08-10 21:14:54 -0400

    club doorman

---
+++ 08-10-2021codewars.js
@@ -0,0 +1,23 @@
+/* Club Doorman:
+The Club Doorman will give you a word. To enter the Club you need to provide the right number according to provided the word.
+
+Every given word has a doubled letter, like 'tt' in lettuce.
+
+To answer the right number you need to find the doubled letter's position of the given word in the alphabet and multiply this number per 3.
+
+It will be given only words with one doubled letter.
+
+
+EXAMPLE
+Lettuce is the given word. 't' is the doubled letter and it's position is 20 in the alphabet.
+
+The answer to the Club Doorman is 20 * 3 = 60 */
+
+function passTheDoorMan (word){
+    let alphaString = "abcdefghijklmnopqrstuvwxyz"
+    for (let i = 0; i < word.length; i++) {
+      if (word[i] === word[i+1]) {
+        return (alphaString.indexOf(word[i])+1) * 3
+      }
+    }
+  }
