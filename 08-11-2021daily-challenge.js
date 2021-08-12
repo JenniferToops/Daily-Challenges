5c1fc6b3f4853d2b9d828da5826156b52520c605
Jennifer Toops <jennifertoops@gmail.com>
2021-08-11 23:10:52 -0400

    sort numbers 7 kyu

---
+++ 08-11-2021daily-challenge.js
@@ -0,0 +1,23 @@
+/*Given a string of characters, return the character that appears the most often.
+
+describe("Max Character", () => {
+ it("Should return max character", () => {
+  assert.equal(max("Hello World!"), "l");
+ });
+});
+
+No String or Array Methods (well brute force it first, but then no methods)! */
+
+//=================
+//=================NOT DONE YET
+//=================
+
+/* 
+Sort Numbers 
+Finish the solution so that it sorts the passed in array of numbers. If the function passes in an empty array or null/nil value then it should return an empty array.
+
+For example:
+
+solution([1, 2, 10, 50, 5]); // should return [1,2,5,10,50]
+solution(null); // should return []*/
+const solution = nums => nums === null ? [] : nums.sort((a, b) => a - b)
