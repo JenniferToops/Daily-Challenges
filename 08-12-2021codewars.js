78a15f63b155598661f694ca0c30ab8ec70f2042
Jennifer Toops <jennifertoops@gmail.com>
2021-08-12 10:58:39 -0400

    multiples of 3 or 5

--- 08-12-2021codewars.js
+++ 08-12-2021codewars.js
@@ -21,3 +21,28 @@ function removeUrlAnchor(url){
     }
   return truncatedUrl
 }
+
+// use replace() or split()
+
+function removeUrlAnchor(url) {
+    return url.replace(/#.+/ig, "");
+  }
+
+function removeUrlAnchor(u){
+    return u.split('#')[0].toString();
+  }
+
+  /* multiples of 3 or 5 */
+  
+  function solution(number){
+    let multiplesArr = []
+    if (number === 0) {
+      return 0
+    } else {
+      for (let i = 0; i < number; i++) {
+      if (i % 3 === 0 || i % 5 === 0) {
+        multiplesArr.push(i)
+        }
+      }
+    } return multiplesArr.reduce((acc, c) => acc + c, 0)
+  }
