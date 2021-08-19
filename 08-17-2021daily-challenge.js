76e7286ce0c6114a1231ac57cd16c6eaa69b85d3
Jennifer Toops <jennifertoops@gmail.com>
2021-08-19 00:05:19 -0400

    vaporcode

---
+++ 08-17-2021daily-challenge.js
@@ -0,0 +1,17 @@
+/*
+describe("Reverse Words", () => {
+ it("Should reverse words", () => {
+  assert.equal(reverseWords("I love JavaScript!"), "I evol !tpircSavaJ");
+ });
+});
+
+No Reverse Method! Unless that is your brute force...*/
+
+function reverseWords(str) {
+    //given a string of words. There may be spaces, other characters and punctuation
+    //return the string with the words in the original order but the characters in the words are in reversed order
+    //if the string is empty, return the empty string
+    //split the string into separate words.
+    //reverse the letters in each of the words and push into another array
+    //join the array taking care to leave spaces between the words
+}
