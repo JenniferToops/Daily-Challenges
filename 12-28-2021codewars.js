03377d77cd7b93f6568dbcce99d5637f5338e608
Jennifer Toops <jennifertoops@gmail.com>
2021-12-28 16:58:26 -0500

    added incrementer (7) codewar

--- 12-28-2021codewars.js
+++ 12-28-2021codewars.js
@@ -12,3 +12,15 @@ For example:
 The input string can be of arbitrary length - in particular, it may be empty. All input is guaranteed to be valid, i.e. each input string will only ever consist of 'G', 'C', 'A' and/or 'T'<div className=""></div>*/
 
 const DNAtoRNA = dna => dna.split("").map(el => el === "T" ? "U" : el).join("")
+
+
+//incrementer
+/* Given an input of an array of digits, return the array with each digit incremented by its position in the array: the first digit will be incremented by 1, the second digit by 2, etc. Make sure to start counting your positions from 1 (and not 0).
+
+Your result can only contain single digit numbers, so if adding a digit with it's position gives you a multiple-digit number, only the last digit of the number should be returned.
+
+Notes:
+return an empty array if your array is empty
+arrays will only contain numbers so don't worry about checking that*/
+
+const incrementer = nums => nums.length < 1 ? [] : nums.map((el, i) => Number((el + (i + 1)).toString().slice(-1)))
